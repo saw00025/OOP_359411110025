a2541b8d7d9ba1bb802076d558af4f6652038eef
saw <nongsow4002@gmail.com>
2019-11-28 11:18:04 +0700

    delete

--- src/Lab1/Hello.java
+++ src/Lab1/Hello.java
@@ -3,7 +3,7 @@ package Lab1;
 public class Hello {
     public static void main(String[] args) {
     System.out.println("Hello World,I'm saw");
-    System.out.println("Information System");
+
 
 
     }
