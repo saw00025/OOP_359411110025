8aae2b767561fdae4a381013c7160f0d8c9468d3
saw00025 <58280114+saw00025@users.noreply.github.com>
2019-12-12 09:16:39 +0700

    Update Hello.java

--- src/Lab1/Hello.java
+++ src/Lab1/Hello.java
@@ -3,9 +3,9 @@ package Lab1;
 public class Hello {
     public static void main(String[] args) {
     System.out.println("Hello World,I'm saw");
+    System.out.println("@ saiyai");
 
 
-
-    }
-}
+    }//main
+}//class
 
