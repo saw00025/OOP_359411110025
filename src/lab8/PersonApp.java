e7b1b4a6f9372c53ebd795af405c6564a074ff8d
saw <nongsow4002@gmail.com>
2020-02-27 08:43:51 +0700

    lab4 :method12

--- src/lab8/PersonApp.java
+++ src/lab8/PersonApp.java
@@ -1,20 +1,23 @@
-package Lab8;
+package lab8;
 
 public class PersonApp {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         //create objects
-        person P1 = new person("1111111111111",
-                "Montira Kliengtapong",
+        Person P1 = new Person("1111111111111",
+                "Nudchanad Sansom",
                 "09/09/1997");
         System.out.println(P1.toString());
 
-        Student S1 = new Student("2222222222222",
-                "Nudchanad Sansom",
+        student S1 = new student("2222222222222",
+                "Montira Kliengtapong",
                 "15/03/1997",
-                "STD025","Information System");
+                "STD025", "Information System");
 
-        S1.running();
-        S1.addSubject();
+        S1.();
+        S1.add Subject();
         //P1.addSubject();
         // object ของ super-class ไม่สามารถเรียกใช้ method ของ sub-class
     }
+}
+
+
